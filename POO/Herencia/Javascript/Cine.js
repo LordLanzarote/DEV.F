97d93cb65cb06690cad58e630b830f0eba84c55b
LordLanzarote <87791690+LordLanzarote@users.noreply.github.com>
2022-01-27 20:56:35 -0600

    Instanciando objetos de clase

--- POO/Herencia/Javascript/Cine.js
+++ POO/Herencia/Javascript/Cine.js
@@ -5,7 +5,31 @@ import { Partido } from "./Partido";
 
 class Cine {
 
-    constructor(){
-        
+    constructor(sala){
+        this.sala = sala;
+    }
+
+    reproducir(){
+        return this.sala.setMovie();
     }
 }
+
+// INSTANCIAR OBJETO PELICULA
+const titanic = new Pelicula ('TITANIC', '22MIN', 'B');
+const SalaUno = new Sala ('1', '230personas', titanic);
+const cinepolisPabellon = new Cine (SalaUno);
+
+// INSTANCIAR OBJETO DOCUMENTAL
+const documental = new Documental ('Catfish', '50MIN', 'A');
+const SalaDos = new Sala ('2', '230personas', documental);
+const cinepolisGalerias = new Cine (SalaDos);
+
+// INSTANCIAR OBJETO PARTIDO
+const partidoFut = new Partido ('Champions', '125MIN', 'A');
+const SalaTres = new Sala ('3', '270personas', partidoFut);
+const cinemex = new Cine (SalaTres);
+
+// IMPRIMIR LOS OBJETOS DE CLASE
+console.log(cinepolisPabellon.reproducir());
+console.log(cinepolisGalerias.reproducir());
+console.log(cinemex.reproducir());
